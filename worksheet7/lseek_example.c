bc1c4818e67cd837ae2142b33870604d0d7b16ab
Zuberbuhler <m.zuberbuhler.ehs@gmail.com>
2021-03-11 16:00:03 -0800

    Made changes to lseek_example.c

--- worksheet7/lseek_example.c
+++ worksheet7/lseek_example.c
@@ -36,7 +36,6 @@ void func(char arr[], int n)
             // nth byte)
         else
         {
-            count = (n);
             lseek(f_read, count, SEEK_CUR);
             write(f_write, arr, 1);
         }
