4fc9dd99cd80ed1e28e69831e0cc6b3c7ebf48dd
Zuberbuhler <m.zuberbuhler.ehs@gmail.com>
2021-04-07 18:54:17 -0700

    modified timeExample.c

--- assignment4/timeExample.c
+++ assignment4/timeExample.c
@@ -1,41 +1,46 @@
 /* Program to demonstrate time taken by function fun() */
-#include <stdio.h> 
-#include <time.h> 
-  
-// A function that terminates when enter key is pressed 
-void fun() 
-{ 
-    printf("fun() starts \n"); 
-    printf("Press enter to stop fun \n"); 
-    while(1) 
-    { 
-        if (getchar()) 
-            break; 
-    } 
-    printf("fun() ends \n"); 
-} 
-  
-// The main program calls fun() and measures time taken by fun() 
-int main() 
-{ 
-    // Calculate the time taken by fun() 
-    struct timespec start, finish;
-    double elapsed;
+#include <stdio.h>
+#include <time.h>
 
-    clock_t t; 
-    t = clock(); 
+// A function that terminates when enter key is pressed
+void fun()
+{
+    printf("fun() starts \n");
+    printf("Press enter to stop fun \n");
+    while(1)
+    {
+        if (getchar())
+            break;
+    }
+    printf("fun() ends \n");
+}
+
+// The main program calls fun() and measures time taken by fun()
+int main()
+{
+    // Calculate the time taken by fun()
+    struct timespec start = {0,0}, finish = {0,0};
+    double elapsed = 0;
+
+    clock_t t;
+    t = clock();
     clock_gettime(CLOCK_MONOTONIC, &start);
-    printf("start %ld\n", start.tv_sec);
-    fun(); 
+    printf("start %ld\n", start.tv_nsec);
+    fun();
 
     clock_gettime(CLOCK_MONOTONIC, &finish);
-    printf("finish %ld\n", finish.tv_sec);
-    //printf("CLOCKSPERSEC %ld\n", CLOCKS_PER_SEC);
-    //double time_taken = ((double)t)*1000.0/CLOCKS_PER_SEC; // in seconds 
-    elapsed = (finish.tv_sec - start.tv_sec);
-    //Alternative with more precision:
-    //elapsed += (finish.tv_nsec - start.tv_nsec) / 1000000000.0;
- 
-    printf("fun() took %f seconds to execute \n", elapsed); 
-    return 0; 
-} 
+    printf("finish %ld\n", finish.tv_nsec);
+
+    elapsed = ((double)finish.tv_sec + 1.0e-9*finish.tv_nsec) -
+                ((double)start.tv_sec + 1.0e-9*start.tv_nsec);
+    if(elapsed <= 2)
+    {
+        printf("less than 2 seconds!!!\n");
+    }
+    printf("fun() took %.5f seconds to execute \n", elapsed);
+    printf("fun() took about %.5f seconds\n",
+           ((double)finish.tv_sec + 1.0e-9*finish.tv_nsec) -
+           ((double)start.tv_sec + 1.0e-9*start.tv_nsec));
+
+    return 0;
+}
